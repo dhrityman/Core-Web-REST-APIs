d852be38fd7b0e781f484252fb82ca64a5a28476
Dhrityman Mukherjee <dhrityman.technical@gmail.com>
2026-08-09 13:38:10 +0100

    add a project for API Version Control

---
+++ ApiVersioning/ApiVersioning/Dto/EmployeeDto.cs
@@ -0,0 +1,41 @@
+﻿using ApiVersioning.Entities;
+using System.ComponentModel.DataAnnotations;
+
+namespace ApiVersioning.Dto
+{
+    public class EmployeeDto
+    {
+        public EmployeeDto()
+        {
+        }
+        public EmployeeDto(Employee employee)
+        {
+            if (employee != null)
+            {
+                this.Id = employee.Id;
+                this.Name = employee.Name;
+                this.CreatedDate = employee.CreatedDate;
+                this.LastModifieddDate = employee.LastModifieddDate;
+                this.DOB = employee.DOB;
+                this.Position = employee.Position;
+                this.Department = employee.Department;
+                this.EmailAddress = employee.EmailAddress;
+            }
+        }
+        public Guid Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public DateTime? CreatedDate { get; set; }
+
+        public DateTime? LastModifieddDate { get; set; }
+
+        public DateOnly? DOB { get; set; }
+
+        public string? Position { get; set; }
+
+        public string? Department { get; set; }
+
+        public string? EmailAddress { get; set; }
+    }
+}
