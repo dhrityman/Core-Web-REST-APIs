77c2698d687618e6a041eca2ec692a3ba652c5ee
Dhrityman Mukherjee <dhrityman.technical@gmail.com>
2026-09-03 13:19:53 +0100

    Update EventManagement project for Create Event

--- EventManagement/EventManagementWebApi/Models/Dto/EventDto.cs
+++ EventManagement/EventManagementWebApi/Models/Dto/EventDto.cs
@@ -4,10 +4,13 @@ namespace EventManagementWebApi.Models
 {
     public class EventDto
     {
+        //[Required(ErrorMessage = "Title is required")]
         [Required]
         public string Title { get; set; }
+        //[MaxLength(500, ErrorMessage = "Description is can not be more than 500 charecters")]
         [MaxLength(500)]
         public string Description { get; set; }
+       // [Required(ErrorMessage = "Start Date is required")]
         [Required]
         public DateTime StartDate { get; set; }
         public DateTime? EndDate { get; set; }
